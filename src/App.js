8d1d3c055bfdbfb812ce01e61d2fd533b6999021
peetpan <peetamber.p@somaiya.edu>
2021-11-21 22:13:49 +0530

    HomePage with Routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/homepage/homepage.component';
+import {Switch, Route} from 'react-router-dom'
+
+const HatsPage = () => (
+  <div> <h1>HATS PAGE</h1></div>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Switch>
+      <div >
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/shop/hats' component={HatsPage} />
+      </div>
+    </Switch>
+    );
 }
 
 export default App;
