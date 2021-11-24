8343f89e8f39a559021df6aacb14b5619a95d81e
peetpan <peetamber.p@somaiya.edu>
2021-11-24 17:15:27 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
+import Header from './components/header/header.component';
 import {Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <Switch>
-      <div >
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
-      </div>
-    </Switch>
+    <div>
+      <Header/>
+      <Switch>
+        <div >
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+        </div>
+      </Switch>
+    </div>
     );
 }
 
