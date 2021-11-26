c6d448dfbeae54f507ff718ba1675c3a1dceb2fa
peetpan <peetamber.p@somaiya.edu>
2021-11-26 09:20:06 +0530

    signin

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import {Switch, Route} from 'react-router-dom'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <div >
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInAndSignUp} />
         </div>
       </Switch>
     </div>
